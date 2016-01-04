6f7147678aebefc0ffdb7d61842fc8e4b87ae114
Hassan <hassan.abid@nexstreaming.com>
2016-01-04 15:55:33 +0900

    Initial Commit

---
+++ Search Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search Restaurant
+//
+//  Created by NexStreamingCorp on 1/4/16.
+//  Copyright © 2016 NexStreamingCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
